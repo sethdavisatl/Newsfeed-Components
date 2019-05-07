78da2c11460868a48f844d8655e331bf3669d202
Seth Davis <seth@sethdavisatl.com>
2019-05-07 15:33:20 -0400

    MVP finished

--- Article/Article.js
+++ Article/Article.js
@@ -1,22 +1,27 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
+    // assign this.domElement to the passed in domElement	    // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.document.querySelectorAll('.expandButton');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    // create a reference to the ".expandButton" class. 	    // create a reference to the ".expandButton" class. 
+    this.expandButton = this.domElement.querySelector(".expandButton");
+    // Using your expandButton reference, update the text on your expandButton to say "expand"	    // Using your expandButton reference, update the text on your expandButton to say "expand"
+
+    this.expandButton.innerText = "expand";
+    // Set a click handler on the expandButton reference, calling the expandArticle method.	    // Set a click handler on the expandButton reference, calling the expandArticle method.
+
+    this.expandButton.addEventListener("click", () => this.expandArticle());
   }
+
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    // Using our reference to the domElement, toggle a class to expand or hide the article.	    // Using our reference to the domElement, toggle a class to expand or hide the article.
 
+
+    this.domElement.classList.toggle('article-open');
   }
 }
+
 
 /* START HERE: 
 
@@ -26,4 +31,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(article => {
+  return new Article(article);
+});
