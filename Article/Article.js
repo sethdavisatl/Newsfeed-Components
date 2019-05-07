0f932e59591e0d36ceb091c76e12fe2b328d11eb
Seth Davis <seth@sethdavisatl.com>
2019-05-07 15:12:30 -0400

    import less files

--- Article/Article.js
+++ Article/Article.js
@@ -3,11 +3,11 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.document.querySelectorAll('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
